1c43ad0fbc59eea200d3ae5a3446331ec6f85e07
Leo Jeong <80350771+33tm@users.noreply.github.com>
2024-10-30 20:08:32 -0700

    fetch grades + types

--- src/app/(app)/grades/[id]/page.tsx
+++ src/app/(app)/grades/[id]/page.tsx
@@ -1,21 +1,66 @@
 "use client"
 
-import { useContext } from "react"
-import { useParams } from "next/navigation"
+import { useContext, useState } from "react"
+import { notFound, useParams } from "next/navigation"
 import { useGrades } from "@/hooks/useGrades"
 
 import { AuthContext } from "@/contexts/AuthContext"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import { ArrowLeft } from "lucide-react"
 
 export default function Course() {
     const { id } = useParams<{ id: string }>()
     const { user } = useContext(AuthContext)
-    const { grades, error, refresh } = useGrades(id)
+    const { grades, error, refreshing, refresh } = useGrades(id)
+
+    const { periods, scales } = grades.data
+    const [period, setPeriod] = useState(periods[0])
+
+    console.log(period)
+
+    if (!user) {
+        return (
+            <></>
+        )
+    }
+
+    const section = user.sections.find(section => section.id === id)
+
+    if (!section) {
+        return (
+            <></>
+        )
+    }
 
     return (
         <>
             <title>
-                {`${user?.sections.find(section => section.id === id)?.name} | Grades | Gunn One`}
+                {`${section.name} | Grades | Gunn One`}
             </title>
+            <div className="flex fixed md:relative bottom-20 md:bottom-0 mx-8">
+                <div className="space-y-1">
+                    <Link href="/grades" className="flex text-sm text-muted-foreground opacity-75 hover:underline">
+                        <ArrowLeft size={13} className="my-auto mr-2" /> All Courses
+                    </Link>
+                    <div className="text-xl font-extrabold">
+                        <h1 className="md:text-3xl">{section.name}</h1>
+                        <h1 className="md:text-2xl text-muted-foreground">{section.period} {section.teacher}</h1>
+                    </div>
+                </div>
+                <p className="text-xl md:text-3xl text-muted-foreground font-bold">{period.grade}%</p>
+            </div>
+            {/* {refreshing && "Refreshing..."}
+            {period && period.categories.map(category => (
+                <div key={category.id}>
+                    <p>{category.name}</p>
+                    {category.items.map(item => (
+                        <div key={item.id}>
+                            <p>{item.name}</p>
+                        </div>
+                    ))}
+                </div>
+            ))} */}
         </>
     )
 }
--- src/hooks/useGrades.ts
+++ src/hooks/useGrades.ts
@@ -1,38 +1,92 @@
-import { useState, useEffect, useContext } from "react"
+import { useState, useEffect, useContext, useCallback } from "react"
 import { AuthContext } from "@/contexts/AuthContext"
+
+interface Grades {
+    data: {
+        periods: {
+            id: string
+            name: string
+            grade: number
+            scale: string
+            categories: {
+                id: number
+                name: string
+                weight: number
+                grade: number
+                items: {
+                    id: number
+                    name: string
+                    due: string
+                    url: string
+                    grade: number
+                    max: number
+                    scale: number
+                }[]
+            }[]
+        }[]
+        scales: {
+            id: number
+            title: string
+            type: number
+            auto_averaging: number
+            hide_numeric: number
+            scale: {
+                level: {
+                    grade: string
+                    cutoff: number
+                    average: number
+                }[]
+            }
+        }[]
+    }
+    timestamp: number
+}
 
 export function useGrades(id: string) {
     const { user } = useContext(AuthContext)
     const [error, setError] = useState<string>()
-    const [grades, setGrades] = useState(getCached())
+    const [grades, setGrades] = useState<Grades>(getCached())
+    const [refreshing, setRefreshing] = useState(false)
 
     function getCached() {
+        const defaultGrades = {
+            data: {
+                periods: [],
+                scales: []
+            },
+            timestamp: 0
+        } satisfies Grades
         try {
             const cached = localStorage.getItem(`${id}-grades`)
-            if (!cached) return null
+            if (!cached) return defaultGrades
             return JSON.parse(cached)
         } catch {
-            return null
+            return defaultGrades
         }
     }
 
     function refresh() {
         if (!user) return
+        setRefreshing(true)
         fetch(`${process.env.NEXT_PUBLIC_API_URL}/sections/${id}/grades`, {
             method: "POST",
             credentials: "include"
         }).then(async res => {
             if (res.status === 200) {
-                const grades = await res.json()
+                const grades = {
+                    data: await res.json(),
+                    timestamp: Date.now()
+                } satisfies Grades
                 setGrades(grades)
                 localStorage.setItem(`${id}-grades`, JSON.stringify(grades))
             } else {
                 setError("Error")
             }
+            setRefreshing(false)
         })
     }
 
     useEffect(refresh, [user])
 
-    return { grades, error, refresh }
+    return { grades, error, refreshing, refresh }
 }
