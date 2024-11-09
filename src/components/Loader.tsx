e4381a11bd82ca5a25cdb00af221b7dc4327ca42
Leo Jeong <80350771+33tm@users.noreply.github.com>
2024-11-08 22:19:56 -0800

    mobile centering

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,7 +2,7 @@ import { CgSpinner } from "react-icons/cg"
 
 export function Loader() {
     return (
-        <div className="flex fixed top-0 h-screen w-screen -z-10">
+        <div className="flex fixed top-0 h-dvh w-screen -z-10">
             <CgSpinner className="m-auto text-4xl animate-spin" />
         </div>
     )
