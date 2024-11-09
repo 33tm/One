a7fbd39387eadcbc741d66ca7881950306081b7c
Leo Jeong <80350771+33tm@users.noreply.github.com>
2024-11-08 22:46:32 -0800

    verify ip against pausd cidr

--- src/app/oauth/page.tsx
+++ src/app/oauth/page.tsx
@@ -10,7 +10,8 @@ export default async ({ searchParams }: { searchParams: Promise<Record<string, s
     if (!token)
         return redirect("/")
 
-    if (ip.startsWith("199.80.")) {
+    // PAUSD CIDR: 199.80.192.0/18
+    if (ip.match(/^199\.80\.(?:19[2-9]|2[0-4][0-9]|25[0-5])\.(?:\d{1,2}|1\d{2}|2[0-4]\d|25[0-5])$/)) {
         const params = new URLSearchParams({
             oauth_token: token,
             oauth_callback: `${process.env.NEXT_PUBLIC_CALLBACK_URL}?domain=${encodeURIComponent("https://pausd.schoology.com")}`
