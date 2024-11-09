448948fe3a943138d2961cb6852a81da1dbd695b
Leo Jeong <80350771+33tm@users.noreply.github.com>
2024-11-08 22:47:03 -0800

    external school search

--- src/app/oauth/oauth.tsx
+++ src/app/oauth/oauth.tsx
@@ -8,19 +8,33 @@ import { redirect } from "@/server/redirect"
 
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-
+import { Separator } from "@/components/ui/separator"
+import { ChevronLeft, Circle } from "lucide-react"
 
 export function OAuth({ token }: { token: string }) {
     const [query, setQuery] = useState("")
     const [schools, setSchools] = useState<School[]>()
     const [school, setSchool] = useState<School | null>(null)
+    const [redirecting, setRedirecting] = useState(false)
+    const [loading, setLoading] = useState(false)
     const [open, setOpen] = useState(false)
 
     useEffect(() => {
-        if (query.length < 3) return
-        setSchool(null)
-        search(query).then(setSchools)
+        if (query.length < 3)
+            return setSchools(undefined)
+        setLoading(true)
+        const timeout = setTimeout(() => {
+            search(query)
+                .then(setSchools)
+                .then(() => setLoading(false))
+        }, 200)
+        return () => clearTimeout(timeout)
     }, [query])
+
+    useEffect(() => {
+        if (!schools?.find(s => s.id === school?.id))
+            setSchool(null)
+    }, [schools])
 
     function oauth(domain: string) {
         const params = new URLSearchParams({
@@ -31,48 +45,89 @@ export function OAuth({ token }: { token: string }) {
     }
 
     return (
-        <div className="h-screen">
-            <div className="flex h-1/2">
-                <Link href={oauth("pausd.schoology.com")}>
-                    asdsdfjkdskakjl
-                </Link>
-                <Input
-                    onChange={({ target }) => setQuery(target.value)}
-                    onFocus={() => setOpen(true)}
-                />
-                <Button
-                    disabled={!school}
-                    onClick={() => redirect(oauth(school?.domain || "app.schoology.com"))}
-                >
-                    Open
-                </Button>
+        <div className="overflow-hidden">
+            <div className={`absolute w-screen transition-opacity duration-200 opacity-0 ${open && "opacity-100"}`}>
+                {open && (
+                    <div className="flex p-4">
+                        <Button
+                            variant="secondary"
+                            className="rounded-r-none"
+                            onClick={() => open && setOpen(false)}
+                        >
+                            <ChevronLeft />
+                        </Button>
+                        <Input
+                            autoFocus
+                            className="text-lg rounded-none"
+                            placeholder="Search"
+                            defaultValue={query}
+                            onChange={({ target }) => setQuery(target.value)}
+                        />
+                        <Button
+                            className="rounded-l-none"
+                            disabled={!school || loading || redirecting}
+                            onClick={() => {
+                                setRedirecting(true)
+                                redirect(oauth(school?.domain || "app.schoology.com"))
+                            }}
+                        >
+                            Open
+                        </Button>
+                    </div>
+                )}
+                <div className="h-auto px-4 pb-4 space-y-2 overflow-y-auto overflow-x-hidden select-none">
+                    {schools && schools[0] && schools[0].title !== "No Schools Found" ? (
+                        schools.filter(({ id }) => id !== 2573996462).map(s => (
+                            <div
+                                key={s.id + s.title}
+                                className={`p-3 rounded-lg font-bold hover:scale-[101.5%] hover:shadow-2xl hover:cursor-pointer transition duration-200 ${((s.id === school?.id || s.location === school?.location) && (s.domain === school?.domain && s.domain !== "app.schoology.com")) ? "bg-primary text-tirtiary" : "bg-tirtiary"}`}
+                                onClick={() => setSchool(s)}
+                            >
+                                <p className="text-xl truncate">{s.title}</p>
+                                <p className="text-secondary truncate">{s.location}</p>
+                                <p className="text-secondary truncate">{s.domain || "app.schoology.com"}</p>
+                            </div>
+                        ))
+                    ) : (
+                        query.length < 3 ? (
+                            <></>
+                        ) : loading ? (
+                            "Loading..."
+                        ) : (
+                            "No Schools"
+                        )
+                    )}
+                </div>
             </div>
-            <div className="h-1/2">
-                <div className="h-full p-2 space-y-2 overflow-y-auto">
-                    {schools && schools[0].title !== "No Schools Found" ? (
-                        schools.filter(({ id }) => id !== 2573996462).map(s => {
-                            const [city, state] = s.location.split(", ") || []
-                            console.log(school?.domain)
-                            return (
-                                <div
-                                    key={s.id + s.title}
-                                    className={`flex p-2 rounded-lg hover:scale-105 hover:shadow-2xl hover:cursor-pointer transition duration-200 ${((s.id === school?.id || s.location === school?.location) && (s.domain === school?.domain && s.domain !== "app.schoology.com")) ? "bg-primary text-tirtiary" : "bg-tirtiary"}`}
-                                    onClick={() => setSchool(s)}
-                                >
-                                    <div className="font-bold">
-                                        <p className="text-xl max-w-80 truncate">{s.title}</p>
-                                        <p className="text-secondary">{s.domain || "app.schoology.com"}</p>
-                                    </div>
-                                    <div className="flex ml-auto text-secondary">
-                                        <div className="my-auto">
-                                            <p className="max-w-24 truncate text-right font-bold">{city}</p>
-                                            <p className="max-w-24 truncate text-right">{state}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            )
-                        })
-                    ) : "No schools found!"}
+            <div className={`flex h-[50dvh] transition-transform duration-500 ${open && "-translate-y-full"}`}>
+                <Circle strokeWidth={4} size={32} className="m-auto" />
+            </div>
+            <div className={`h-[50dvh] transition-transform duration-500 ${open && "translate-y-full"}`}>
+                <div className="absolute bottom-0 w-screen">
+                    <div
+                        className="flex h-[22dvh] bg-tirtiary text-lg font-semibold rounded-t-2xl hover:cursor-pointer"
+                        onClick={() => setOpen(true)}
+                    >
+                        <p className="m-auto">
+                            Find My School
+                        </p>
+                    </div>
+                    <div className={`flex flex-col bg-tirtiary h-[6dvh]`}>
+                        <div className={`flex justify-between h-[100px] bg-background rounded-t-2xl`}>
+                            <Separator className="my-auto w-[40vw]" />
+                            <p className="my-auto text-secondary font-bold">OR</p>
+                            <Separator className="my-auto w-[40vw]" />
+                        </div>
+                    </div>
+                    <Link
+                        href={oauth("pausd.schoology.com")}
+                        className="flex h-[22dvh] bg-primary text-lg font-bold text-tirtiary rounded-t-2xl"
+                        prefetch
+                    >
+                        <p className="m-auto">
+                            PAUSD Login
+                        </p>
+                    </Link>
                 </div>
             </div>
         </div >
