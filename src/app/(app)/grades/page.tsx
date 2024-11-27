96de12df112fde5e39602e8491f8df4239021142
33tm <80350771+33tm@users.noreply.github.com>
2024-11-26 16:05:18 -0800

    mobile grades screen

--- src/app/(app)/grades/page.tsx
+++ src/app/(app)/grades/page.tsx
@@ -17,26 +17,25 @@ export default function Grades() {
                 Grades | Gunn One
             </title>
             {user ? (
-                <div className="mb-24 md:mb-0 md:p-24 mx-3 md:mx-0 grid grid-cols-1 md:grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-3">
-                    <div className="md:hidden flex h-[50dvh]">
-                        <p className="m-auto text-xl font-semibold">Grades</p>
+                <div className="h-full w-full overflow-y-auto overflow-x-hidden">
+                    <div className="m-4 md:m-8 grid grid-cols-1 md:grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-3">
+                        {user?.sections.map((section) => (
+                            <Link key={section.id} href={`/grades/${section.id}`}>
+                                <div className="relative group text-center text-lg py-8 rounded-xl hover:scale-105 hover:shadow-2xl transition ease-out duration-200">
+                                    <p className="font-bold truncate px-8">{section.name}</p>
+                                    <p className="truncate px-8">{section.section}</p>
+                                    <Image
+                                        fill
+                                        priority
+                                        sizes="170px"
+                                        src={section.image}
+                                        alt={section.name}
+                                        className="-z-10 rounded-xl object-cover opacity-20 group-hover:opacity-40 transition-opacity duration-200"
+                                    />
+                                </div>
+                            </Link>
+                        ))}
                     </div>
-                    {user?.sections.map((section) => (
-                        <Link key={section.id} href={`/grades/${section.id}`}>
-                            <div className="relative group text-center text-lg py-8 rounded-xl md:hover:scale-105 md:hover:shadow-2xl transition ease-out duration-200">
-                                <p className="font-bold truncate px-8">{section.name}</p>
-                                <p className="truncate px-8">{section.section}</p>
-                                <Image
-                                    fill
-                                    priority
-                                    sizes="170px"
-                                    src={section.image}
-                                    alt={section.name}
-                                    className="-z-10 rounded-xl object-cover opacity-20 md:group-hover:opacity-40 transition-opacity duration-200"
-                                />
-                            </div>
-                        </Link>
-                    ))}
                 </div>
             ) : (
                 <div className="flex fixed top-0 -z-10 w-screen h-screen outline-dashed">
