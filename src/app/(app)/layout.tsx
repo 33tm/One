275eb4ade2790a9a9bfc785ca9b9c7d4bf60b497
33tm <80350771+33tm@users.noreply.github.com>
2024-11-26 16:05:30 -0800

    navbar layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,24 +1,13 @@
-"use client"
-
-import { useEffect } from "react"
-import { usePathname } from "next/navigation"
-
-import { toast } from "sonner"
-
 import { Navbar } from "@/components/Navbar"
 import { Toaster } from "@/components/ui/sonner"
 
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
-    const pathname = usePathname()
-
-    useEffect(() => {
-        toast.dismiss()
-    }, [pathname])
-
     return (
         <>
             <Navbar />
-            {children}
+            <div className="-z-20 h-[calc(100dvh-72px)] w-screen">
+                {children}
+            </div>
             <Toaster />
         </>
     )
