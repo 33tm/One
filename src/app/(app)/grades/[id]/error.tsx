d8c27ffccba5b27b7574210dee328831b574c32f
Leo Jeong <80350771+33tm@users.noreply.github.com>
2024-12-17 13:20:26 -0800

    handle broken grade cache

---
+++ src/app/(app)/grades/[id]/error.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { useParams } from "next/navigation"
+import { useEffect } from "react"
+
+import { Error } from "@/components/Error"
+
+export default () => {
+    const { id } = useParams<{ id: string }>()
+
+    useEffect(() => {
+        if (!id) return
+        localStorage.removeItem(`grades-${id}`)
+    }, [id])
+
+    return (
+        <Error>
+            An error occurred!
+        </Error>
+    )
+}
