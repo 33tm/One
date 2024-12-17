81ad4e7887f815cb4bd817caa2218e94adaf5280
Leo Jeong <80350771+33tm@users.noreply.github.com>
2024-12-17 13:22:53 -0800

    api endpoint

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,13 +3,13 @@
 export default function server(uri: string, options?: RequestInit) {
     const url = process.env.NODE_ENV === "development"
         ? window.location.origin.replace("3000", "443")
-        : process.env.NEXT_PUBLIC_API_URL
+        : `${window.location.origin}/api`
     return fetch(`${url}${uri}`, options)
 }
 
 export function websocket(uri: string) {
     const url = process.env.NODE_ENV === "development"
         ? window.location.origin.replace("3000", "443")
-        : process.env.NEXT_PUBLIC_API_URL!
+        : `${window.location.origin}/api`
     return new WebSocket(`${url.replace("http", "ws")}${uri}`)
 }
