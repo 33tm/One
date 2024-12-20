1b6d870e7ab77440afd930206092930e1bc0bab2
Leo Jeong <80350771+33tm@users.noreply.github.com>
2024-12-19 17:12:07 -0800

    Update index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,13 +3,13 @@
 export default function server(uri: string, options?: RequestInit) {
     const url = process.env.NODE_ENV === "development"
         ? window.location.origin.replace("3000", "443")
-        : `${window.location.origin}/api`
+        : `${process.env.NEXT_PUBLIC_API_URL}`
     return fetch(`${url}${uri}`, options)
 }
 
 export function websocket(uri: string) {
     const url = process.env.NODE_ENV === "development"
         ? window.location.origin.replace("3000", "443")
-        : `${window.location.origin}/api`
+        : `${process.env.NEXT_PUBLIC_API_URL}`
     return new WebSocket(`${url.replace("http", "ws")}${uri}`)
 }
