162f84a7c305b45fa57ace45bbb57ed4e1bc6ef5
Leo Jeong <80350771+33tm@users.noreply.github.com>
2025-05-19 17:48:52 -0700

    Update AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -101,7 +101,7 @@ export const AuthProvider = ({ children }: Readonly<{ children: React.ReactNode 
 
     return (
         <AuthContext.Provider value={{ user, loading, auth, refresh, logout }}>
-            {error ? <Error>Server upgrade in progress ~5:00PM PDT</Error> : children}
+            {error ? <Error>Server offline!</Error> : children}
         </AuthContext.Provider>
     )
 }
